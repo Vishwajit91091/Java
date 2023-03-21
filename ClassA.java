31ed86b73cc8bac358805320f1d12cf81701b5ce
Vishwajit Kumar <125690952+Vishwajit91091@users.noreply.github.com>
2023-03-22 00:33:47 +0530

    Add files via upload

--- ClassA.java
+++ ClassA.java
@@ -1,15 +1,27 @@
-package com.pack2;
+package garbagecollector;
 
-public class ClassA implements InterfaceA,InterfaceB
+public class ClassA 
 {
-	public void meth1() {
-		InterfaceA.super.meth1();
-		InterfaceB.super.meth1();
+	void meth1()
+	{
+		System.out.println("meth1() called");
+		ClassA aobj=new ClassA(); //(3rd) All the objs present inside the methods
+	}
+	protected  void finalize()
+	{
+		System.out.println("garbage has been collected ");
 	}
 
-	public static void main(String[] args) {
-		InterfaceA aobj=new ClassA();
-		aobj.meth1();
+	public static void main(String[] args) 
+	{
+		ClassA aobj1=new ClassA();
+		ClassA aobj2=new ClassA();
+		aobj1.meth1();
+		//aobj1=null; //(1st) if we r nullifying the refrance
+		//aobj1.meth1();// raises a NullPointerException
+		//aobj2=aobj1; //(2nd) if we r assining the refrance to another refrance
+		//aobj1.meth1();
+		System.gc();
 
 	}
 
